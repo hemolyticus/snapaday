af6b5561f28dff97ea5cd2b9d037cf4196c19952
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-01-03 15:44:30 +1300

    Alert injected in constructor

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { PhotoModel } from '../../models/photo-model';
+import { SimpleAlert } from '../../providers/simple-alert'
+
 
 
 @Component({
@@ -9,7 +11,8 @@ import { PhotoModel } from '../../models/photo-model';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public simpleAlert: SimpleAlert)
+  {
     
   }
 
