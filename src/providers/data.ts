f52b3be9909dc86f0e20472932ddb151bbf42962
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-01-05 10:48:52 +1300

    Implement Data Service

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,18 +1,26 @@
+import { Storage } from '@ionic/storage';
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the Data provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class Data {
 
-  constructor(public http: Http) {
-    console.log('Hello Data Provider');
+  constructor(public storage: Storage) {
+
+  }
+
+  getData():Promise<any>
+  {
+
+      return this.storage.get('photos');
+
+  }
+
+  save(data): void
+  {
+
+      let newData = JSON.stringify(data);
+      this.storage.set('photos', newData);
+
   }
 
 }
