3a54169b829015425b1356d5753bb5b1867118e7
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-01-05 10:35:58 +1300

    Implement removePhoto();

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -179,6 +179,25 @@ export class HomePage {
   removePhoto(photo): void
   {
 
+      let today = new Date();
+
+      if (photo.date.setHours(0,0,0,0) === today.setHours(0,0,0,0))
+      {
+
+          this.photoTaken=false;
+
+      }
+
+      let index = this.photos.indexOf(photo);
+
+
+      if (index > -1)
+      {
+
+          this.photos.splice(index, 1);
+          this.save();
+
+      }
 
 
   }
